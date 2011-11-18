d89e81a84080af649c8e5af1114ec86bcb59e8f1
Frederico Araujo <fred.the.master@gmail.com>
2011-11-18 17:50:44 +0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Serenity::Application.config.secret_token = '55627ab3f0a3660cede9cbcf57fb679be57593f3a3fa35b39da4ce1b9f3a65d5a9c4cb60256f8e6867a4c0a3fcfd135e8a1eb1f20d957a19cc340739333eb464'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Serenity::Application.config.session_store :cookie_store, :key => '_serenity_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Serenity::Application.config.session_store :active_record_store
